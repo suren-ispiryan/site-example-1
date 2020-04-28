6640db73eacac2a9ad737f12f81bd7249c4d9a2a
Suren <suren.ispiryan2016@gmail.com>
2020-04-28 22:12:59 +0400

    Adding an js, JQuery-cdn and some hovers to css ;)

---
+++ main.js
@@ -0,0 +1,28 @@
+var goUpBtn = document.getElementById("goUpButton");
+
+document.addEventListener("scroll", function(){
+	goUpBtn.style.display = "block";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
