4513ec3abb79935d5ee63fc47fe238297b41a138
Suren <suren.ispiryan2016@gmail.com>
2020-05-02 18:13:18 +0400

    a little debug in js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 			/* go up button on scroll appear */
 			
-var goUpBtn = document.getElementById("goUpButton");
+var goUpBtn = document.getElementById("go-up-button");
 
 document.addEventListener("scroll", function(){
 	goUpBtn.style.display = "block";
